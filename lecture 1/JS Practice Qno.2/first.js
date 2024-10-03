9c6c038b5f3bbc840fce52e7ed2b27a36d9da641
ALINA AFZAL <161754754+ALINA-AFZAL@users.noreply.github.com>
2024-10-03 10:57:39 -0700

    Add files via upload

---
+++ lecture 1/JS Practice Qno.2/first.js
@@ -0,0 +1,12 @@
+const profile = {
+    userName: "@shradhakhapra",
+    isFolllow: false,
+    followers: 569,
+    following: 1,
+
+};
+console.log(typeof profile ["userName"]);
+console.log(typeof profile ["isFollow"]);
+console.log(typeof profile ["followers"]);
+console.log(typeof profile ["following"]);
+
